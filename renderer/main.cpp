2cb4b05b3800caef835867c2f17e1ab48fbd67ff
jelo <wqw85@sina.com>
2017-10-17 19:33:46 +0800

    add glm

---
+++ renderer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  renderer
+//
+//  Created by jelo on 10/14/17.
+//  Copyright © 2017 jelo. All rights reserved.
+//
+
+#include <iostream>
+#include "glm/glm.hpp"
+int main(int argc, const char * argv[]) {
+
+    return 0;
+
+}
